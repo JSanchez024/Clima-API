40fa44e612fa2ced81c26913f71bbd17e3e6bf88
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2026-05-25 13:11:44 -0400

    Clima-consumo-API v.1

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,75 @@
+import { useState, type ChangeEvent, type FormEvent } from 'react';
+import type { SearchType } from '../../types';
+import { countries } from '../../data/countries';
+import styles from './Form.module.css';
+
+export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) =>
+    {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmite = ( e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(Object.values(search).includes('')){
+            console.log('si hay campos vacios...')
+        }
+    }
+
+
+
+  return (
+    <form 
+        className={styles.form}
+        onSubmit={handleSubmite}
+        >
+        <div className={styles.field}>
+            <label htmlFor='city'>Ciudad:</label>
+            <input
+                id='city'
+                type='text'
+                name='city'
+                placeholder='Ciudad'
+                value={search.city}
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className={styles.field}>
+            <label htmlFor="country">Pais:</label>
+            <select 
+                id='"country' 
+                value={search.country}
+                name='county'
+                onChange={handleChange}
+            >
+                <option value="">--- Seleccione un Pais ---</option>
+                {countries.map( country => (
+                    <option
+                    key={country.code}
+                    value={country.code}
+                    >
+                        {country.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+            <input className={styles.submit} type='submit' value='Consultar Clima' />
+    </form>
+
+
+
+
+
+  )
+}
